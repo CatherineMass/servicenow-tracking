a7852948cb766c8209f63229157b37ddb11bf032
Catherine Massarelli <catherine.massarelli@gmail.com>
2023-04-06 12:57:40 +0200

    Add weather api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const data = require("./data.json")
+const fetch = require("node-fetch-commonjs")
 const app = express()
+
 const PORT = process.env.PORT || 6700
 
 app.get('/', (req, res) => {
-  res.send("The API is running, yay yay!!!")
+  res.send("The API is running, yay yay!!!");
 })
 
 app.get('/tracking/:nb', (req, res) => {
@@ -13,6 +15,22 @@ app.get('/tracking/:nb', (req, res) => {
   const result = data.filter(d => d._id === nb)
 
   res.send(result)
+})
+
+app.get('/weather', async (req, res) => {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=59&lon=18&appid=de838abc989f8e5b62217294a1fe83ee&units=metric')
+  const data = await response.json()
+
+  const weatherObj = {
+    location: "Stockholm",
+    desc: data.weather[0].desc,
+    windspd: data.wind.speed,
+    temp: data.main.temp,
+    feels_like: data.main.feels_like,
+    icon: data.weather[0].icon
+  }
+
+  res.send(weatherObj)
 })
 
 app.listen(PORT, () => {
